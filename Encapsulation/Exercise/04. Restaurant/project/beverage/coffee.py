1147f1f75cb568b687f6fe63e9af40109e3cb340
hristoiliev <hristoiliev.1994@gmail.com>
2023-03-15 12:25:27 +0200

    labs

--- Encapsulation/Exercise/04. Restaurant/project/beverage/coffee.py
+++ Encapsulation/Exercise/04. Restaurant/project/beverage/coffee.py
@@ -2,11 +2,11 @@ from project.beverage.hot_beverage import HotBeverage
 
 
 class Coffee(HotBeverage):
-    MILLILITERS = 50
-    PRICE = 3.50
+    MILLILITERS: int = 50
+    PRICE: float = 3.50
 
-    def __init__(self, name: str, price: float, caffeine: float):
-        super().__init__(name, price, Coffee.PRICE, Coffee.MILLILITERS)
+    def __init__(self, name: str, caffeine: float):
+        super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
         self.__caffeine = caffeine
 
     @property
